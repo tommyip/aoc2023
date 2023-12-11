801dac480d9dff16ed6fb08c171c54aaabfac678
Thomas Ip <thomas@ipthomas.com>
2023-12-11 12:11:12 +0000

    Python: Day 7 clean up

--- python/day07.py
+++ python/day07.py
@@ -3,47 +3,12 @@ from collections import Counter
 with open('inputs/day07.txt') as f:
     lines = [line.split(' ') for line in f.read().splitlines()]
 
-# part1 = 0
-# strength = '23456789TJQKA'
-# cards = []
-# for hand, multiplier in lines:
-#     hand_strength = sorted([strength.index(h) for h in hand], reverse=True)
-#     # print(hand_strength)
-#     strength_idx = Counter(hand_strength).most_common()
-#     # print(strength_idx)
-#     rank = []
-#     for i in range(5):
-#         kind = 5 - i
-#         # rank.append(
-#         #     max([s for (s, c) in strength_idx if c == kind], default=0))
-#         rank.append(tuple(s for (s, c) in strength_idx if c == kind))
-#     cards.append(tuple(rank))
-
-# print(cards)
-
-# print([x[1]
-#        for x in sorted(enumerate(cards), reverse=True, key=lambda x: x[1])])
-# orders = [len(cards) - x[0]
-#           for x in sorted(enumerate(cards), reverse=True, key=lambda x: x[1])]
-# print(orders)
-
-# print(sum(int(multiplier) * order
-#       for (_, multiplier), order in zip(lines, orders)))
-
-
-part1 = 0
 strength = '23456789TJQKA'
-cards = []
+cards1 = []
 for hand, multiplier in lines:
-    hand_strength = sorted([strength.index(h) for h in hand], reverse=True)
-    # print(hand_strength)
+    hand_strength = [strength.index(h) for h in hand]
     strength_idx = Counter(hand_strength).most_common()
-    # print(strength_idx)
-    rank = []
-    for i in range(5):
-        kind = 5 - i
-        rank.append(len(tuple(s for (s, c) in strength_idx if c == kind)))
-    rank = tuple(rank)
+    rank = tuple(sum(c == 5 - i for (_, c) in strength_idx) for i in range(5))
     r = None
     if rank[0] == 1:
         r = 6
@@ -60,33 +25,22 @@ for hand, multiplier in lines:
     else:
         r = 0
 
-    cards.append((r, *[strength.index(h) for h in hand]))
-
+    cards1.append((r, *[strength.index(h) for h in hand]))
 
 multipliers = [int(multiplier)
-               for card, (_, multiplier) in sorted(zip(cards, lines), reverse=True, key=lambda x: x[0])]
-# print(sum(int(multiplier) * order
-#       for (_, multiplier), order in zip(lines, orders)))
+               for card, (_, multiplier) in sorted(zip(cards1, lines), reverse=True, key=lambda x: x[0])]
 print(sum((len(multipliers) - i) * multiplier for i,
       multiplier in enumerate(multipliers)))
 
-
-part1 = 0
 strengthj = 'J23456789TQKA'
-cards = []
+cards2 = []
 for hand, multiplier in lines:
     j_count = sum(h == 'J' for h in hand)
-    hand_strength = sorted([strength.index(h) for h in hand if h != 'J'], reverse=True)
+    hand_strength = [strength.index(h) for h in hand if h != 'J']
     m = Counter(hand_strength).most_common()[0][0] if len(hand_strength) > 0 else len(strength)-1
-    hand_strength = ([m]*j_count)+hand_strength
-    # print(hand_strength)
+    hand_strength = ([m] * j_count) + hand_strength
     strength_idx = Counter(hand_strength).most_common()
-    # print(strength_idx)
-    rank = []
-    for i in range(5):
-        kind = 5 - i
-        rank.append(len(tuple(s for (s, c) in strength_idx if c == kind)))
-    rank = tuple(rank)
+    rank = tuple(sum(c == 5 - i for (_, c) in strength_idx) for i in range(5))
     r = None
     if rank[0] == 1:
         r = 6
@@ -102,15 +56,9 @@ for hand, multiplier in lines:
         r = 1
     else:
         r = 0
-    cards.append((r, *[strengthj.index(h) for h in hand]))
+    cards2.append((r, *[strengthj.index(h) for h in hand]))
 
-print(cards)
-multipliers = [card
-               for card, (_, multiplier) in sorted(zip(cards, lines), reverse=True, key=lambda x: x[0])]
-print(multipliers)
 multipliers = [int(multiplier)
-               for card, (_, multiplier) in sorted(zip(cards, lines), reverse=True, key=lambda x: x[0])]
-# print(sum(int(multiplier) * order
-#       for (_, multiplier), order in zip(lines, orders)))
+               for card, (_, multiplier) in sorted(zip(cards2, lines), reverse=True, key=lambda x: x[0])]
 print(sum((len(multipliers) - i) * multiplier for i,
       multiplier in enumerate(multipliers)))
