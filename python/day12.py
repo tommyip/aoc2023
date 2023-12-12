086c8d74b91b1546e697f811e58c883bc12f911f
Thomas Ip <thomas@ipthomas.com>
2023-12-12 16:40:46 +0000

    Python: Day 12 clean up

--- python/day12.py
+++ python/day12.py
@@ -7,26 +7,31 @@ with open('inputs/day12.txt') as f:
 def solve(records, r_i, groups, g_i):
     @lru_cache
     def aux(r_i, g_i):
+        # Base cases
         if r_i == len(records):
             return int(g_i == len(groups))
         if g_i == len(groups):
             return all(c in '.?' for c in records[r_i:])
         if sum(groups[g_i:]) + len(groups) - g_i - 1 > len(records) - r_i:
             return 0
+
+        # Recursive cases
+        # Case 1: Advance til not .
         if records[r_i] == '.':
             r_i += 1
             while r_i < len(records) and records[r_i] == '.':
                 r_i += 1
             return aux(r_i, g_i)
+
+        # Case 2: Set ?s as #
         arrangements = 0
         group_len = groups[g_i]
-        # Make the header #s
         header = records[r_i:r_i+group_len]
         if all(c in '?#' for c in header):
             if r_i + group_len == len(records) or records[r_i + group_len] in '?.':
                 arrangements += aux(r_i + group_len + 1, g_i + 1)
 
-        # Make the header .s
+        # Case 3: Set ? as .
         if records[r_i] == '?':
             arrangements += aux(r_i + 1, g_i)
         return arrangements
@@ -37,12 +42,7 @@ part2 = 0
 for line in lines:
     records, groups = line.split(' ')
     groups = [int(x) for x in groups.split(',')]
-    overlap = solve(records, 0, groups, 0)
-    part1 += overlap
-
-    rrecords = '?'.join(repeat(records, 5))
-    rgroups = groups * 5
-    overlap5 = solve(rrecords, 0, rgroups, 0)
-    part2 += overlap5
+    part1 += solve(records, 0, groups, 0)
+    part2 += solve('?'.join(repeat(records, 5)), 0, groups * 5, 0)
 
 print(part1, part2)
