bd0354c3b3b3d4d064d7a224944e0fbc4affdcd9
Thomas Ip <thomas@ipthomas.com>
2023-12-24 22:28:04 +0000

    Python: Day 24 😬

--- python/day24.py
+++ python/day24.py
@@ -1,4 +1,5 @@
-from itertools import combinations
+import numpy as np
+from itertools import combinations, product
 
 with open('inputs/day24.txt') as f:
     lines = [line.split(' @ ') for line in f.read().splitlines()]
@@ -8,57 +9,72 @@ MAX = 400000000000000
 
 stones = []
 for pos, vel in lines:
-    pos = [int(x) for x in pos.split(', ')]
-    vel = [int(x) for x in vel.split(', ')]
+    pos = np.array([int(x) for x in pos.split(', ')])
+    vel = np.array([int(x) for x in vel.split(', ')])
     stones.append((pos, vel))
 
 
-def hail_intersection(a0, a1, b0, b1):
-    denom = ((a0.real - a1.real) * (b0.imag - b1.imag) -
-             (a0.imag - a1.imag) * (b0.real - b1.real))
-    if denom == 0:
-        return None
-    num0 = a0.real * a1.imag - a0.imag * a1.real
-    num1 = b0.real * b1.imag - b0.imag * b1.real
-    x = num0 * (b0.real - b1.real) - (a0.real - a1.real) * num1
-    y = num0 * (b0.imag - b1.imag) - (a0.imag - a1.imag) * num1
-    return complex(x / denom, y / denom)
-
-
-def hail_intersection2(a0, a1, b0, b1):
-    x1, x2, x3, x4 = a0.real, a1.real, b0.real, b1.real
-    y1, y2, y3, y4 = a0.imag, a1.imag, b0.imag, b1.imag
-    numerator = ((x1 - x3) * (y3 - y4)) - ((y1 - y3) * (x3 - x4))
-    denominator = ((x1 - x2) * (y3 - y4)) - ((y1 - y2) * (x3 - x4))
-    if denominator == 0:
-        return None
-    t = numerator / denominator
-    if not (0 <= t <= 1):
-        return None
-    px = x1 + t * (x2 - x1)
-    py = y1 + t * (y2 - y1)
-    return complex(px, py)
-
-
 def is_future(pos, vel, intersect):
-    # t = (intersect - pos) / vel
-    # return t.real >= 0 and t.imag >= 0
-    t = (intersect.real - pos.real) / vel.real
-    return t >= 0
+    return (intersect[0] - pos[0]) / vel[0] >= 0
+
+
+def intersection(u_pos, u_vel, v_pos, v_vel):
+    x, err, rank = np.linalg.lstsq(
+        np.array([u_vel.T, -v_vel.T]).T, v_pos.T-u_pos.T, rcond=None)[:3]
+    if rank == 2:
+        return u_pos + u_vel * x[0]
 
 
 part1 = 0
 for (a_pos, a_vel), (b_pos, b_vel) in combinations(stones, r=2):
-    a0 = complex(*a_pos[:2])
-    av = complex(*a_vel[:2])
-    b0 = complex(*b_pos[:2])
-    bv = complex(*b_vel[:2])
-    intersect = hail_intersection(a0, a0+av*MAX, b0, b0+bv*MAX)
-    # print(a_pos, b_pos, intersect)
-    if intersect is not None and (MIN <= intersect.real <= MAX and MIN <= intersect.imag <= MAX):
-        afuture = is_future(a0, av, intersect)
-        bfuture = is_future(b0, bv, intersect)
-        if afuture and bfuture:
+    a_pos_xy = np.array([*a_pos[:2], 0])
+    a_vel_xy = np.array([*a_vel[:2], 0])
+    b_pos_xy = np.array([*b_pos[:2], 0])
+    b_vel_xy = np.array([*b_vel[:2], 0])
+    intersect = intersection(a_pos_xy, a_vel_xy, b_pos_xy, b_vel_xy)
+    if intersect is not None and np.all((intersect[:2] >= MIN) & (intersect[:2] <= MAX)):
+        if is_future(a_pos_xy, a_vel_xy, intersect) and is_future(b_pos_xy, b_vel_xy, intersect):
             part1 += 1
 
 print(part1)
+
+
+BF_MAX = 300
+
+xs = set(range(-BF_MAX, BF_MAX+1))
+ys = set(range(-BF_MAX, BF_MAX+1))
+zs = set(range(-BF_MAX, BF_MAX+1))
+
+
+def prune_vels(ax, av, bx, bv, vs):
+    if ax > bx and av > bv:
+        for v in range(bv, av+1):
+            if v in vs:
+                vs.remove(v)
+
+
+for (a_pos, a_vel), (b_pos, b_vel) in combinations(stones, r=2):
+    prune_vels(a_pos[0], a_vel[0], b_pos[0], b_vel[0], xs)
+    prune_vels(a_pos[1], a_vel[1], b_pos[1], b_vel[1], ys)
+    prune_vels(a_pos[2], a_vel[2], b_pos[2], b_vel[2], zs)
+
+
+s0_pos, s0_vel = stones[0]
+s1_pos, s1_vel = stones[1]
+
+
+for x, y, z in product(xs, ys, zs):
+    r_vel = np.array([x, y, z])
+    sr0_vel = s0_vel - r_vel
+    sr1_vel = s1_vel - r_vel
+    intersect0 = intersection(s0_pos, sr0_vel, s1_pos, sr1_vel)
+    if intersect0 is None:
+        continue
+    for s_pos, s_vel in stones[2:]:
+        sr_vel = s_vel - r_vel
+        intersect = intersection(s0_pos, sr0_vel, s_pos, sr_vel)
+        if intersect is None or not np.allclose(intersect, intersect0):
+            break
+    else:
+        print(round(intersect0.sum()))
+        break
